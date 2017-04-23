e31ca9eca0d161253561f26e3797f1292a5a74b4
Eirik Haustveit <eirhau19@ad.hfk.no>
2017-04-23 23:32:28 +0200

    Added linker .cmd for flash

--- templates/basic-template/main.c
+++ templates/basic-template/main.c
@@ -37,6 +37,12 @@
 #include "f2802x_common/include/pll.h"
 #include "f2802x_common/include/wdog.h"
 
+#include "f2802x_common/include/flash.h"
+#include "f2802x_common/include/pie.h"
+#include "f2802x_common/include/adc.h"
+#include "f2802x_common/include/sci.h"
+#include "f2802x_common/include/sci_io.h"
+
 #include "main.h"
 
 void main()
@@ -57,16 +63,32 @@ void main()
 
     CLK_Handle myClk;
     PLL_Handle myPll;
+    CPU_Handle myCpu;
 
     myClk = CLK_init((void *)CLK_BASE_ADDR, sizeof(CLK_Obj));
     myPll = PLL_init((void *)PLL_BASE_ADDR, sizeof(PLL_Obj));
+    myCpu = CPU_init((void *)NULL, sizeof(CPU_Obj));
 
+    ADC_Handle myAdc;
+    FLASH_Handle myFlash;
+    PIE_Handle myPie;
+    SCI_Handle mySci;
+
+    myFlash = FLASH_init((void *)FLASH_BASE_ADDR, sizeof(FLASH_Obj));
+    myPie = PIE_init((void *)PIE_BASE_ADDR, sizeof(PIE_Obj));
+    mySci = SCI_init((void *)SCIA_BASE_ADDR, sizeof(SCI_Obj));
+    myAdc = ADC_init((void *)ADC_BASE_ADDR, sizeof(ADC_Obj));
 
     //Select the internal oscillator 1 as the clock source
     CLK_setOscSrc(myClk, CLK_OscSrc_Internal);
 
     PLL_setup(myPll, PLL_Multiplier_12, PLL_DivideSelect_ClkIn_by_2);
 
+    // Disables the peripheral interrupt expansion (PIE)
+    PIE_disable(myPie);
+    PIE_disableAllInts(myPie);
+    CPU_disableGlobalInts(myCpu);
+    CPU_clearIntFlags(myCpu);
 
     /*
      * We have four blue LED's connected to the GPIO of the controller
